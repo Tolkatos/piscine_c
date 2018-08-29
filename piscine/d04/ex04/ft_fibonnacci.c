9e88700e1ea982e94280e1b5b077c72bed082c10
Raphael Nuytten <raphael.nuytten@gmail.com>
2018-08-29 16:36:15 +0200

    Premier essai

---
+++ piscine/d04/ex04/ft_fibonnacci.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonnacci.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ranuytte <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/29 16:22:21 by ranuytte          #+#    #+#             */
+/*   Updated: 2018/08/29 16:34:23 by ranuytte         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int		ft_fibonnaci(int index)
+{
+	if (index < 0)
+		return (-1);
+	if (index == 0)
+		return (0);
+	if (index == 1)
+		return (1);
+	return (index + ft_fibonnaci(index - 1));
+}
+
+int		main(void)
+{
+	printf("%d", ft_fibonnaci(6));
+	return (0);
+}
+
