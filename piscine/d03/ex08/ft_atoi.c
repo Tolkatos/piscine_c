1515a4574982a369f4249a74a2433275220d53a2
Raphael Nuytten <raphael.nuytten@gmail.com>
2018-08-29 09:15:56 +0200

    Correction norminette

--- piscine/d03/ex08/ft_atoi.c
+++ piscine/d03/ex08/ft_atoi.c
@@ -6,23 +6,24 @@
 /*   By: ranuytte <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/28 16:05:49 by ranuytte          #+#    #+#             */
-/*   Updated: 2018/08/29 09:07:39 by ranuytte         ###   ########.fr       */
+/*   Updated: 2018/08/29 09:15:28 by ranuytte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int	ft_atoi(char *str)
+int		ft_atoi(char *str)
 {
-	int i;
-	int s;
-	int r;
+	int		i;
+	int		s;
+	int		r;
 
 	i = 0;
 	s = 1;
 	r = 0;
-	while (str[i] == '\n' || str[i] == '\t' || str[i] == '\r' || str[i] == '\v' || str[i] == ' ' || str[i] == '\f')
+	while (str[i] == '\n' || str[i] == '\t' || str[i] == '\r' \
+			|| str[i] == '\v' || str[i] == ' ' || str[i] == '\f')
 		i++;
 	if (str[i] == '-')
 	{
@@ -37,13 +38,12 @@ int	ft_atoi(char *str)
 		r += str[i] - '0';
 		i++;
 	}
-	return (r * s);	
+	return (r * s);
 }
 
 int		main(int argc, char **argv)
 {
 	argc = 0;
-//	(void)argc;
 	printf("%d", ft_atoi(argv[1]));
 	printf("%s", "\n");
 	printf("%d", atoi(argv[1]));
