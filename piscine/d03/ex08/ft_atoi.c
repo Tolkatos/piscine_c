8038d88bad4f75e50d86ff47f22b963097d030a1
Raphael Nuytten <raphael.nuytten@gmail.com>
2018-08-29 09:11:18 +0200

    Ajout incrementation dans le dernier while

--- piscine/d03/ex08/ft_atoi.c
+++ piscine/d03/ex08/ft_atoi.c
@@ -6,11 +6,14 @@
 /*   By: ranuytte <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/28 16:05:49 by ranuytte          #+#    #+#             */
-/*   Updated: 2018/08/29 08:50:03 by ranuytte         ###   ########.fr       */
+/*   Updated: 2018/08/29 09:07:39 by ranuytte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_atoi(char *str)
+#include <stdio.h>
+#include <stdlib.h>
+
+int	ft_atoi(char *str)
 {
 	int i;
 	int s;
@@ -19,29 +22,30 @@ int		ft_atoi(char *str)
 	i = 0;
 	s = 1;
 	r = 0;
-	while (str[i] == '\n' || str[i] == '\t'|| str[i] == '\r' || str[i] == '\v' || str[i] == ' ' || str[i] == '\f')
-		i++
+	while (str[i] == '\n' || str[i] == '\t' || str[i] == '\r' || str[i] == '\v' || str[i] == ' ' || str[i] == '\f')
+		i++;
 	if (str[i] == '-')
 	{
 		i++;
-		s = -1
+		s = -1;
 	}
 	else if (str[i] == '+')
 		i++;
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		r *= 10;
-		r += *str - '0';
-	}		
+		r += str[i] - '0';
+		i++;
+	}
 	return (r * s);	
 }
 
 int		main(int argc, char **argv)
 {
-	argc = O;
+	argc = 0;
+//	(void)argc;
 	printf("%d", ft_atoi(argv[1]));
 	printf("%s", "\n");
 	printf("%d", atoi(argv[1]));
-	return 0;
+	return (0);
 }
-
