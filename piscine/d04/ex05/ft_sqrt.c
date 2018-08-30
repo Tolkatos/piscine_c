279eee8efcde07af9e1f565086bae8461bcda95b
Raphael Nuytten <raphael.nuytten@gmail.com>
2018-08-30 09:02:08 +0200

    Correction norme ok

--- piscine/d04/ex05/ft_sqrt.c
+++ piscine/d04/ex05/ft_sqrt.c
@@ -6,29 +6,28 @@
 /*   By: ranuytte <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/29 16:37:01 by ranuytte          #+#    #+#             */
-/*   Updated: 2018/08/29 18:24:22 by ranuytte         ###   ########.fr       */
+/*   Updated: 2018/08/30 09:01:07 by ranuytte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 int		ft_sqrt(int nb)
 {
 	int		i;
 
 	i = 1;
 	if (nb == 0)
-		return(0);
-
+		return (0);
 	while (nb > i * i)
 		i++;
-
 	if (nb == i * i)
 		return (i);
-	return(0);
-}
-
-int		main(void)
-{
-	printf("%d", ft_sqrt(26));
 	return (0);
 }
+
+/*
+**int		main(void)
+**{
+**	printf("%d", ft_sqrt(26));
+**	return (0);
+**}
+*/
