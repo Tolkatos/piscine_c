121278eba6555ab39d9667e77e5495c3b2d57ccb
Raphael Nuytten <raphael.nuytten@gmail.com>
2018-08-30 08:59:16 +0200

    Correction norminette ok

--- piscine/d04/ex06/ft_is_prime.c
+++ piscine/d04/ex06/ft_is_prime.c
@@ -6,33 +6,30 @@
 /*   By: ranuytte <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/29 18:26:36 by ranuytte          #+#    #+#             */
-/*   Updated: 2018/08/29 18:54:02 by ranuytte         ###   ########.fr       */
+/*   Updated: 2018/08/30 08:58:32 by ranuytte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 int		ft_is_prime(int nb)
 {
 	int		i;
 
 	i = 2;
-	if (nb == 0 || nb == 1)
-		return (0);
-	if (nb != 2 && (nb % 2 == 0 || nb % 2 == 5))
+	if (nb <= 1)
 		return (0);
 	while (i <= nb)
 	{
-		if (nb % i != 1)
+		if (nb % i == 0 && nb != i)
 			return (0);
-		else
-			return (1);
 		i++;
 	}
 	return (1);
 }
 
-int		main(void)
-{
-	printf("%d", ft_is_prime(18));
-	return (0);
-}
+/*
+**int		main(void)
+**{
+**	printf("%d", ft_is_prime(30122));
+**	return (0);
+**}
+*/
