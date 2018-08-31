42bdf04134df4abc4e62702efc0d6ec250a942dd
Raphael Nuytten <raphael.nuytten@gmail.com>
2018-08-31 09:43:44 +0200

    Correction exception

--- piscine/d04/ex03/ft_recursive_power.c
+++ piscine/d04/ex03/ft_recursive_power.c
@@ -6,7 +6,7 @@
 /*   By: ranuytte <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/29 13:24:04 by ranuytte          #+#    #+#             */
-/*   Updated: 2018/08/30 09:05:31 by ranuytte         ###   ########.fr       */
+/*   Updated: 2018/08/31 09:38:15 by ranuytte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@ int		ft_recursive_power(int nb, int power)
 {
 	if (power < 0)
 		return (0);
+	else if (power == 0)
+		return (1);
 	else if (power == 1)
 		return (nb);
 	return (nb * ft_recursive_power(nb, power - 1));
