fdb64fbfa8fde1f42f2568a897a7e577cbc12227
Raphael Nuytten <raphael.nuytten@gmail.com>
2018-09-03 18:36:29 +0200

    Premier essai without norme

---
+++ piscine/d07/ex03/ft_concat_params.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_concat_params.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ranuytte <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/03 14:53:17 by ranuytte          #+#    #+#             */
+/*   Updated: 2018/09/03 18:35:54 by ranuytte         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+#include <stdlib.h>
+
+int		calc_len_params(int argc, char **argv)
+{
+	int		i;
+	int		j;
+	int 	r;
+
+	r = 0;
+	i = 0;
+	j = 0;
+	while (i < argc - 1)
+	{
+		i++;
+		while (argv[i][j++])
+			r++;
+		j = 0;
+	}
+	return (r);
+}
+
+char	*ft_concat_params(int argc, char **argv)
+{
+	int		i;
+	int		j;
+	char	*r;
+	int		k;
+
+	k = 0;
+	r = 0;
+	j = 0;
+	i = calc_len_params(argc, argv);
+	r = (char*)malloc(sizeof(*r) * (i + argc));
+	if (r == NULL)
+		return (0);
+	i = 0;
+	while (j < argc - 1)
+	{
+		j++;
+		while (argv[j][i])
+			r[k++] = argv[j][i++];
+		i = 0;
+		if (i < argc - 1)
+			r[k] = '\n';
+		else
+			r[k] = '\0';
+		k++;
+	}
+	return (r);	
+}
+
+int		main(int argc, char **argv)
+{
+	printf("%s", ft_concat_params(argc, argv));
+	return (0);
+}
