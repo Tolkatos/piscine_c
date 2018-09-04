b1e9692e9ea1310f79a35e430cf6b699ff00833d
Raphael Nuytten <raphael.nuytten@gmail.com>
2018-09-04 21:19:46 +0200

    Premier essai without norme

---
+++ piscine/d07/ex05/ft_print_words_tables.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_words_tables.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ranuytte <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/04 20:26:01 by ranuytte          #+#    #+#             */
+/*   Updated: 2018/09/04 21:19:10 by ranuytte         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+char	**ft_split_whitespaces(char *str);
+
+void	ft_print_words_tables(char **tab)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	while (tab[i])
+	{
+		while(tab[i][j])
+		{
+			ft_putchar(tab[i][j]);
+			j++;
+		}
+		j = 0;
+		ft_putchar('\n');
+		i++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	argc = 0;
+	ft_print_words_tables(ft_split_whitespaces(argv[1]));
+	return (0);
+}
+
