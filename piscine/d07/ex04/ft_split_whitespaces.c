8360a49ac469515f1e72af543879eab6ed9d7372
Raphael Nuytten <raphael.nuytten@gmail.com>
2018-09-04 18:46:32 +0200

    Correction segfault et ajout ft_fill_in

--- piscine/d07/ex04/ft_split_whitespaces.c
+++ piscine/d07/ex04/ft_split_whitespaces.c
@@ -6,7 +6,7 @@
 /*   By: ranuytte <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/03 20:17:58 by ranuytte          #+#    #+#             */
-/*   Updated: 2018/09/04 18:01:30 by ranuytte         ###   ########.fr       */
+/*   Updated: 2018/09/04 18:44:53 by ranuytte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
@@ -29,7 +29,7 @@ char	**ft_calc_nbr_word(char *str, char **tab)
 			nb++;
 		}
 	}
-	printf("%d", nb);
+//	printf("%d", nb);
 	tab = (char**)malloc(sizeof(*tab) * (nb + 1));
 	return (tab);
 }	
@@ -52,12 +52,44 @@ char	**ft_calc_len_word(char *str, char **tab)
 			while (str[i] != '\n' && str[i] != '\t' && str[i] != ' ' && str[i])
 			{
 				i++;
-				j++;
+				k++;
 			}
-			printf("%d", j);
-			tab[k] = (char*)malloc(sizeof(*tab) * j + 1);
-			j = 0;
+//			printf("%d", k);
+			tab[j] = (char*)malloc(sizeof(*tab) * (k + 1));
+			j++;
+			k = 0;
 		}
+	}
+	return (tab);
+}
+
+char	**ft_fill_in(char *str, char **tab)
+{
+	int		i;
+	int		j;
+	int		k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n' || str[i] == '\t' || str[i] == ' ')
+			i++;
+		else
+		{
+			while (str[i] != '\n' && str[i] != '\t' && str[i] != ' ' && str[i])
+			{
+				tab[j][k] = str[i];
+				k++;
+				i++;
+			}
+			tab[j][k + 1] = '\0';
+			printf("%c", tab[0][3]);
+			k = 0;
+			j++;
+		}
+		printf("%c", tab[0][3]);
 	}
 	return (tab);
 }
@@ -69,7 +101,7 @@ char	**ft_split_whitespaces(char *str)
 	tab = NULL;
 	tab = ft_calc_nbr_word(str, tab);
 	tab = ft_calc_len_word(str, tab);
-
+	tab = ft_fill_in(str, tab);
 	return (tab);
 }
 
