04ce452cb6f22767589faf33f01eb59e170486bb
Raphael Nuytten <raphael.nuytten@gmail.com>
2018-09-05 19:34:01 +0200

    OK

---
+++ piscine/d08/ex01/ft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ranuytte <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/05 18:56:13 by ranuytte          #+#    #+#             */
+/*   Updated: 2018/09/05 19:32:36 by ranuytte         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+# include <unistd.h>
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
